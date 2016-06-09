f76d77eaa299dbbefe53d03031b3e13e090ba5a1
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2016-06-10 07:08:06 +0900

    Remote Config Sample

---
+++ app/src/main/java/us/shiroyama/android/remoteconfig/MainActivity.java
@@ -0,0 +1,66 @@
+package us.shiroyama.android.remoteconfig;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final String KEY_COLOR = "color";
+
+    private FirebaseRemoteConfig remoteConfig;
+
+    @BindView(R.id.container)
+    View container;
+
+    @OnClick(R.id.button)
+    void onClickButton(Button button) {
+        fetch();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        FirebaseRemoteConfigSettings remoteConfigSettings = new FirebaseRemoteConfigSettings.Builder()
+                .setDeveloperModeEnabled(BuildConfig.DEBUG)
+                .build();
+
+        remoteConfig = FirebaseRemoteConfig.getInstance();
+        remoteConfig.setConfigSettings(remoteConfigSettings);
+        remoteConfig.setDefaults(R.xml.remote_config_defaults);
+
+        fetch();
+    }
+
+    private void fetch() {
+        long cacheExpiration = 3600;
+        if (remoteConfig.getInfo().getConfigSettings().isDeveloperModeEnabled()) {
+            cacheExpiration = 0;
+        }
+
+        remoteConfig.fetch(cacheExpiration)
+                .addOnSuccessListener(aVoid -> {
+                    Log.d(TAG, "Fetch succeeded.");
+                    remoteConfig.activateFetched();
+
+                    String colorName = remoteConfig.getString(KEY_COLOR);
+                    Log.d(TAG, "colorName: " + colorName);
+                    container.setBackgroundColor(Color.parseColor(colorName));
+                })
+                .addOnFailureListener(e -> Log.e(TAG, "Fetch failed.", e));
+    }
+}
